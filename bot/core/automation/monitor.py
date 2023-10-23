51e437edd82599f98f44e3ae10d167e7f10f99d2
Saifa <saifa_96@SaifadeMacBook-Pro.local>
2023-10-24 02:11:39 +0800

    fix: module import error

--- bot/core/automation/monitor.py
+++ bot/core/automation/monitor.py
@@ -3,22 +3,30 @@ from platform import system
 from mss import mss
 from numpy import array
 from cv2 import resize, cvtColor, COLOR_BGRA2BGR
-from ctypes import windll
+
 
 class Monitor:
     def __init__(self, area: Region):
-        self._scale_factor = 1 if system() == 'Darwin' else windll.shcore.GetScaleFactorForDevice(0) / 100
+        self._scale_factor = self._get_scale_factor()
         self.monitor_area = self._init_monitor_area(area)
         self._region = area
-    
+
+    def _get_scale_factor(self):
+        if system() == "Darwin":
+            return 1
+        else:
+            from ctypes import windll
+
+            return windll.shcore.GetScaleFactorForDevice(0) / 100
+
     def _init_monitor_area(self, area: Region):
         return {
-        "left": int(area.x * self._scale_factor),
-        "top": int(area.y * self._scale_factor),
-        "width": int(area.w * self._scale_factor),
-        "height": int(area.h * self._scale_factor),
+            "left": int(area.x * self._scale_factor),
+            "top": int(area.y * self._scale_factor),
+            "width": int(area.w * self._scale_factor),
+            "height": int(area.h * self._scale_factor),
         }
-    
+
     def _cvt_sct_2_frame(self, grabbed_img):
         img_np = array(grabbed_img)
         img_np_3_channel = cvtColor(img_np, COLOR_BGRA2BGR)
@@ -30,14 +38,15 @@ class Monitor:
         scaled_frame = resize(img_np_3_channel, dsize)
         return scaled_frame
 
-    
     def start(self, callback):
         with mss() as sct:
+
             def get_frame():
                 img = sct.grab(self.monitor_area)
                 return self._cvt_sct_2_frame(img)
+
             callback(get_frame)
-    
+
     def get_frame(self):
         with mss() as sct:
             img = sct.grab(self.monitor_area)
