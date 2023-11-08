a0bd7cd1c43d7f2cbab80ef317abeab1f163ce9f
Saifa <saifa_96@SaifadeMacBook-Pro.local>
2023-11-09 01:16:51 +0800

    test: add Button.test.tsx

---
+++ packages/ui/src/tests/Button.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { Button, GlobalStyles } from "../../lib";
+import type { FC, PropsWithChildren } from "react";
+// import "@testing-library/jest-dom";
+
+const Wrapper: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div>
+      <GlobalStyles />
+      {children}
+    </div>
+  );
+};
+
+test("Check Button", () => {
+  render(<Button>Button</Button>, { wrapper: Wrapper });
+
+  const btnElement = screen.getByText("Button");
+
+  expect(btnElement).toBeInTheDocument();
+});
