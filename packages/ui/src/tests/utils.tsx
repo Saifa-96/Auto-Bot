e884c003e556e62f0cf34d432b9eacca64870955
Saifa <saifa_96@SaifadeMacBook-Pro.local>
2023-11-12 21:17:07 +0800

    chore: change button story

--- packages/ui/src/tests/utils.tsx
+++ packages/ui/src/tests/utils.tsx
@@ -1,7 +1,7 @@
 import { GlobalStyles } from "@lib/styles";
 import type { FC, PropsWithChildren } from "react";
 
-export const renderWithTheme: FC<PropsWithChildren> = ({ children }) => {
+export const RenderWithTheme: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div>
       <GlobalStyles />
